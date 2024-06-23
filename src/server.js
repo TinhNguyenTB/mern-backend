7ccbb2f6854ff6d78eb3d8856e522d6eda7ab4e6
TinhNguyenTB <tinh271103@gmail.com>
2024-06-23 08:24:42 +0700

    create user model

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config()
 const connectDB = require('./config/db')
-
+const router = require('./routes/api');
 
 const app = express();
 app.use(cors());
+app.use('/api', router);
 
 const port = process.env.PORT || 8080;
 
